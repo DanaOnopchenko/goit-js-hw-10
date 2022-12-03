a2f0238e2ff17d3544b2fc4987661b5dc9255f7d
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-12-03 04:25:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+
+fetch('https://restcountries.com/v3.1/name/Ukraine?fields=name,capital,population,flags,languages')
+    .then(response => { 
+        return response.json();
+    }).
+    then(country => { 
+        console.log(country)
+    })
+    .catch(error => { 
+        console.log(error);
+    })
