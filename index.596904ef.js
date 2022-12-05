fa2d634b36f246ad4dfaafcd41d292edadd20b84
DanaOnopchenko <dana.onopchenko@gmail.com>
2022-12-05 20:38:53 +0000

    Deploying to gh-pages from @ DanaOnopchenko/goit-js-hw-10@47b2ab4b1290454cbc6de3521cca1dbcbe0e7d57 🚀

--- index.596904ef.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/Ukraine?fields=name,capital,population,flags,languages").then((function(n){return n.json()})).then((function(n){console.log(n)})).catch((function(n){console.log(n)}));
-//# sourceMappingURL=index.596904ef.js.map
--- index.7f720ce8.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/Ukraine?fields=name,capital,population,flags,languages").then((e=>e.json())).then((e=>{console.log(e)})).catch((e=>{console.log(e)}));
-//# sourceMappingURL=index.7f720ce8.js.map
